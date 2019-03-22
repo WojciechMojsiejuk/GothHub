a80d8c6012c1e2d90c4a719edf85cb91da30da50
Paweł Bąk <pawel.bak98@gmail.com>
2019-03-22 20:06:50 +0100

    Added /login

--- GothHub/logowanie/views.py
+++ GothHub/logowanie/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
 
-from .forms import SignUpForm
+
+from .forms import SignUpForm, LoginForm
 
 def signup(request):
     if request.method == 'POST':
@@ -20,4 +22,30 @@ def signup(request):
     return render(request, 'logowanie/signup.html', {'form': form})
 
 def index(request):
-    return HttpResponse("tu bedzie strona logowania")
+    if request.user.is_authenticated:
+        # user home page needed
+        return HttpResponseRedirect('/admin/')
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('Zalogowano poprawnie')
+            else:
+                form = LoginForm()
+                return render(request, 'logowanie/signup.html', {
+                    'form': form,
+                    'error_message': 'Nie znaleziono użytkownika'
+                })
+        else:
+            form = LoginForm()
+            return render(request, 'logowanie/signup.html', {
+                'form': form,
+                'error_message': 'Nieprawidłowy login lub hasło'
+            })
+    else:
+        form = LoginForm()
+    return render(request, 'logowanie/signup.html', {'form': form})
