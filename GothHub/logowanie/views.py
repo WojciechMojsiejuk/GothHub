4f3fd1738916d2fb043e550bec908f7014751dbc
Paweł Bąk <pawel.bak98@gmail.com>
2019-03-22 11:47:00 +0100

    Added login/join

--- GothHub/logowanie/views.py
+++ GothHub/logowanie/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()  # load the profile instance created by the signal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('login:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'logowanie/signup.html', {'form': form})
+
+def index(request):
+    return HttpResponse("tu bedzie strona logowania")
