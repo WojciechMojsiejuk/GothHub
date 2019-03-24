7bf0d60b5f025259b635db0876514b689915bfd7
Wojciech Mojsiejuk <wojciechmojsiejuk@macbook-pro-wojciech.home>
2019-03-24 19:00:49 +0100

    Pierwszy test jednostkowy

--- GothHub/logowanie/tests.py
+++ GothHub/logowanie/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
+from django.urls import resolve
+from logowanie.views import index
 # Create your tests here.
+
+class LoginPageTest(TestCase):
+    def test_login_url_resolves_to_index_page_view(self):
+        found = resolve('/login/')
+        self.assertEqual(found.func, index)
