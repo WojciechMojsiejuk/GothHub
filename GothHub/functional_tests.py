5e77c4cf4389d604c481e120e8b680bd9cef80f9
Wojciech Mojsiejuk <wojciechmojsiejuk@MacBook-Pro-Wojciech.local>
2019-03-25 08:22:57 +0100

    Testy logowania

--- GothHub/functional_tests.py
+++ GothHub/functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -10,8 +11,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_log_to_site(self):
-        self.browser.get('http://localhost:8000')
-        assert 'Django' in self.browser.title
+        self.browser.get('http://localhost:8000/login/')
+
+        #Title should inform it's a login page
+        #self.assertIn('Login',self.browser.title)
+        
+        #Header should inform it's a login form
+        header_text=self.browser.find_element_by_tag_name('h2').text
+        self.assertIn('Logowanie',header_text)
+        
+        #User provides username
+        username_input = self.browser.find_element_by_id('id_username')
+        username_input.send_keys('JanKowlaski')
+
+        #User provides password
+        password_input = self.browser.find_element_by_id('id_password')
+        password_input.send_keys('Haslo_to_okon')
+
+        #User sends form
+        password_input.send_keys(Keys.ENTER)
         
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- GothHub/logowanie/tests.py
+++ GothHub/logowanie/tests.py
@@ -1,9 +1,28 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+from django.test import Client
+
 from logowanie.views import index
+
 # Create your tests here.
 
 class LoginPageTest(TestCase):
+    
+    # creating instance of a client.
+    def setUp(self): 
+        self.client = Client()
+           
     def test_login_url_resolves_to_index_page_view(self):
         found = resolve('/login/')
         self.assertEqual(found.func, index)
+
+    def test_login_page_returns_correct_html(self):
+        response = self.client.post('/login/', {'username': 'john', 'password': 'smith'})
+        
+        #Login page should contain form where user can provide username and password
+        self.assertIn(b'<form method="post">',response.content)
+        self.assertIn(b'Username',response.content)
+        self.assertIn(b'Password',response.content)
+        self.assertIn(b'<button type="submit">Zaloguj</button>',response.content)
+ 
