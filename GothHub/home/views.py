78cf50ee1c55e87dac2cea1e3a16686e16d9ae57
Wojciech Mojsiejuk <wojciechmojsiejuk@macbook-pro-wojciech.home>
2019-05-03 19:15:10 +0200

    add repositories

--- GothHub/home/views.py
+++ GothHub/home/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+from home.models import Repository
 def home(request):
     return render(request, 'home.html', {})
+
+def repository(request):
+    if request.method == 'POST':
+        Repository.objects.create(name=request.POST.cleaned_data.get('name'),is_public=request.POST.cleaned_data.get('is_public'))
+        return redirect('/')
+    repositories = Repository.objects.all()
+    return render(request,'repository.html',{'repositories':repositories})
