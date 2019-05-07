a7c63ac0d271167f16bfc463e3d4df4a822c29f6
Paweł Bąk <pawel.bak98@gmail.com>
2019-05-07 12:33:31 +0200

    repository form update

--- GothHub/home/views.py
+++ GothHub/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 from .models import Repository
 from .forms import RepoCreationForm
@@ -9,12 +10,13 @@ def home(request):
 def repository(request):
     if request.method == 'POST':
         repositories = RepoCreationForm(request.POST)
-        name = repositories.cleaned_data.get('name')
-        is_public = repositories.cleaned_data.get('is_public')
-        repo = Repository(name=name, is_public=is_public)
-        repo.save()
-        return redirect('/')
+        if repositories.is_valid():
+            repo = repositories.save(commit=True)
+            # repo.name = repositories.cleaned_data.get('name')
+            # repo.is_public = repositories.cleaned_data.get('is_public')
+            # repo.save()
+            # Repository.objects.create(name=name, is_public=is_public)
+            return redirect('/')
     else:
         repositories = RepoCreationForm()
-    repositories = Repository.objects.all()
     return render(request,'repository.html',{'repositories':repositories})
