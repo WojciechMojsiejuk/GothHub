870a0e1cb12e943378fb40d89e527c5264e4450a
Paweł Bąk <pawel.bak98@gmail.com>
2019-05-13 16:53:39 +0200

    working adding repos

--- GothHub/home/models.py
+++ GothHub/home/models.py
@@ -6,6 +6,9 @@ class Repository(models.Model):
     name = models.CharField(max_length=128)
     is_public = models.BooleanField(default=False)
 
+    def __str__(self):
+        return str(self.name)
+
     class Meta():
         verbose_name_plural = "Repositories"
 
--- GothHub/home/views.py
+++ GothHub/home/views.py
@@ -9,14 +9,13 @@ def home(request):
 
 def repository(request):
     if request.method == 'POST':
-        repositories = RepoCreationForm(request.POST)
-        if repositories.is_valid():
-            repo = repositories.save(commit=True)
-            # repo.name = repositories.cleaned_data.get('name')
-            # repo.is_public = repositories.cleaned_data.get('is_public')
-            # repo.save()
-            # Repository.objects.create(name=name, is_public=is_public)
-            return redirect('/')
+        form = RepoCreationForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            is_public = form.cleaned_data.get('is_public')
+            Repository.objects.create(name=name, is_public=is_public)
+            return HttpResponseRedirect('/')
     else:
-        repositories = RepoCreationForm()
-    return render(request,'repository.html',{'repositories':repositories})
+        form = RepoCreationForm()
+        repositories = Repository.objects.all()
+    return render(request, 'repository.html', {'form':form, 'repositories':repositories})
