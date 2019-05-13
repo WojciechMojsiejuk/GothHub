ffa79452bf23589d7e9e3eb826002f02ff180dda
Paweł Bąk <pawel.bak98@gmail.com>
2019-05-13 17:33:22 +0200

    user sees own repositories

--- GothHub/home/views.py
+++ GothHub/home/views.py
@@ -8,15 +8,18 @@ def home(request):
     return render(request, 'home.html', {})
 
 def repository(request):
-    if request.method == 'POST':
-        form = RepoCreationForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data.get('name')
-            is_public = form.cleaned_data.get('is_public')
-            user = request.user
-            Repository.objects.create(name=name, is_public=is_public, owner=user)
-            return HttpResponseRedirect('/')
+    if request.user.is_authenticated:
+        user = request.user
+        if request.method == 'POST':
+            form = RepoCreationForm(request.POST)
+            if form.is_valid():
+                name = form.cleaned_data.get('name')
+                is_public = form.cleaned_data.get('is_public')
+                Repository.objects.create(name=name, is_public=is_public, owner=user)
+                return HttpResponseRedirect('/')
+        else:
+            form = RepoCreationForm()
+            repositories = Repository.objects.filter(owner=user)
+        return render(request, 'repository.html', {'form':form, 'repositories':repositories})
     else:
-        form = RepoCreationForm()
-        repositories = Repository.objects.all()
-    return render(request, 'repository.html', {'form':form, 'repositories':repositories})
+        return HttpResponseRedirect('/')
