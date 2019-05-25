354564bd83b6179a90e3816ff720fae8006c3509
Paweł Bąk <pawelbak@MacBook-Air-Pawe.local>
2019-05-25 13:45:33 +0200

    repository list in user profile

--- GothHub/home/urls.py
+++ GothHub/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('repository', views.repository, name='repository'),
-    path('user/<str:username>', views.user, name='user')
+    path('user/<str:username>/add_repository', views.add_repository, name='add_repository'),
+    path('user/<str:username>', views.user, name='user'),
+    path('user/<str:username>/<str:repository>', views.repository, name='repository')
 ]
