82022b55378aeb9f35f4f9d5265b5e85f7ee6115
Adrian <a.wolanowski98@gmail.com>
2019-06-03 15:17:08 +0200

    ITS ALIVE

--- GothHub/upload/views.py
+++ GothHub/upload/views.py
@@ -2,22 +2,8 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from home.forms import FileUploadForm
 from home.models import File, Repository, Catalog
-#from datetime import datetime
-
-'''
-def upload(request):
-    context = {}
-    if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        fs = FileSystemStorage()
-        name = fs.save(uploaded_file.name, uploaded_file)
-        context['url'] = fs.url(name)
-    return render(request, 'upload.html', context)
-'''
 
 def upload_file(request, username, repository, parental_catalog, catalog):
-    #parsed = urllib.parse.urlparse(request.build_absolute_uri())
-    #print(parsed.path)
     user = request.user
     repo = Repository.objects.get(owner = user, name = repository)
     if parental_catalog == None:
@@ -31,18 +17,10 @@ def upload_file(request, username, repository, parental_catalog, catalog):
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             dir = form.cleaned_data.get('dir')
-            #date = str(datetime.datetime.now().date()) + ' ' + str(datetime.datetime.now().time())
-            #print(date)
-            #print(user)
-            #print(dir)
-            #print(repository)
-            #print(catalog_Id)
             File.objects.create(
             author = user,
             file_name = dir,
             #extension =
-            #content =
-            #date_upload = datetime.strptime(mydate, "%Y-%m-%d"),
             repository_Id = repo,
             catalog_Id = catalog,
             )
