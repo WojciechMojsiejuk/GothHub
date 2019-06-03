83f9db82ae38ac9623b1ec04ddff0772bc8db2b5
Paweł Bąk <pawelbak@MacBook-Air-Pawe.local>
2019-06-03 16:46:47 +0200

    url change

--- GothHub/upload/urls.py
+++ GothHub/upload/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'upload'
 urlpatterns = [
-    path('user/<str:username>/<str:repository>/<str:parental_catalog>/<str:catalog>/upload', views.upload_file, name='upload'),
+    path('', views.upload_file, name='upload'),
 ]
